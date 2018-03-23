e3cb6d74222d497ebf4609ba87847d49597741f6
Liu Yiming <35588354+A0155428B@users.noreply.github.com>
2018-03-23 09:57:49 +0800

    Adds color for tags (#39)
    * Adds color tags
    
    * Adds tests for tag color

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -75,4 +75,13 @@ public class PersonCardHandle extends NodeHandle<Node> {
                 .map(Label::getText)
                 .collect(Collectors.toList());
     }
+
+    public List<String> getTagColorClasses(String tag) {
+        return tagLabels.stream()
+                .filter(label -> label.getText().equals(tag))
+                .map(Label::getStyleClass)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No such tag."));
+    }
 }
+
