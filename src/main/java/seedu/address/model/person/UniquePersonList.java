3ce7508ed6d1a996ce21caa5441cfbebd2ade0ef
zhuleyan <1155077028@link.cuhk.edu.hk>
2018-03-28 12:43:06 +0800

    indentation

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -52,8 +52,9 @@ public class UniquePersonList implements Iterable<Person> {
      */
     public void sort() {
         internalList.sort(
-                Comparator.comparing((Person p) -> p.getName().toString(),
-                        (s1, s2) -> (s1.compareToIgnoreCase(s2) == 0) ? s1.compareTo(s2) : s1.compareToIgnoreCase(s2))
+            Comparator.comparing(
+                (Person p) -> p.getName().toString(),
+                (s1, s2) -> (s1.compareToIgnoreCase(s2) == 0) ? s1.compareTo(s2) : s1.compareToIgnoreCase(s2))
         );
     }
     //@@author
