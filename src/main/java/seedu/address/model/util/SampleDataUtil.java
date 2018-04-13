3188fd485c2663c614138fb7a8f44209a959b829
Liu Yiming <35588354+A0155428B@users.noreply.github.com>
2018-04-14 00:37:06 +0800

    adds meaningful tags for sample data (#242)
    * removes whitespace from portfolio
    
    * adds meaningful tags
    
    * Update test for color tag
    
    * updates GuiTestAssert to accomodate change in sample data tags

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -25,22 +25,22 @@ public class SampleDataUtil {
         return new Person[] {
             new Lead(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), NO_REMARK,
-                getTagSet("friends")),
+                getTagSet("HR")),
             new Contact(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), NO_REMARK,
-                getTagSet("colleagues", "friends")),
+                getTagSet("Important", "HR")),
             new Lead(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), NO_REMARK,
-                getTagSet("neighbours")),
+                getTagSet("DNC")),
             new Lead(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), NO_REMARK,
-                getTagSet("family")),
+                getTagSet("StartUp")),
             new Lead(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), NO_REMARK,
-                getTagSet("classmates")),
+                getTagSet("Entrepreneur")),
             new Contact(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), NO_REMARK,
-                getTagSet("colleagues"))
+                getTagSet("Ecommerce"))
         };
     }
 
