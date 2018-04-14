4e4b1ab848743f5bf24f7fd15ec0f11a44a922fc
davidten <david10tco+1@gmail.com>
2018-04-15 02:54:35 +0800

    Add test coverage for setting location

---
+++ src/test/java/seedu/address/logic/commands/GoogleSetLocationCommandTest.java
@@ -0,0 +1,50 @@
+package seedu.address.logic.commands;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import seedu.address.commons.core.Config;
+import seedu.address.commons.util.ConfigUtil;
+import seedu.address.model.person.Address;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class GoogleSetLocationCommandTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private GoogleSetLocationCommand prepareCommand(Address address){
+        GoogleSetLocationCommand command = new GoogleSetLocationCommand(address);
+        return command;
+    }
+
+    @Test
+    public void constructor_nullAddress_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        prepareCommand(null);
+    }
+
+    @Test
+    public void testGoogleExecuteCommand() throws IOException {
+        Config preConfig = Config.setupConfig();
+        preConfig.setUserLocation(null);
+        assertNull(preConfig.getUserLocation());
+
+        String addressString = "6 College Avenue East, Singapore 138614";
+        Address myAddress = new Address(addressString);
+
+        GoogleSetLocationCommand command = prepareCommand(myAddress);
+        CommandResult cr = command.execute();
+        assertEquals(cr.feedbackToUser, GoogleSetLocationCommand.MESSAGE_SUCCESS);
+
+        Config postConfig = Config.setupConfig();
+        assertEquals(addressString, postConfig .getUserLocation());
+    }
+
+
+
+}
