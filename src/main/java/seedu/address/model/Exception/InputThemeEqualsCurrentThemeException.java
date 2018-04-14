a692896e44132d171614f4e7464f07661274ea95
DESKTOP-9K2NI32\Liu Yiming <e0031565@u.nus.edu>
2018-04-14 13:33:22 +0800

    updates protfolio provides short explanation for unused code

--- src/main/java/seedu/address/model/Exception/InputThemeEqualsCurrentThemeException.java
+++ src/main/java/seedu/address/model/Exception/InputThemeEqualsCurrentThemeException.java
@@ -1,6 +1,7 @@
 package seedu.address.model.exception;
 
 //@@author A0155428B-unused
+//this exception was used in the implementation of change theme command in v1.4
 /**
  * Signals that the input theme is the current theme
  */
